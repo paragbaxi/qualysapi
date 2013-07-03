f4f5e710033d1f100c5faf7e798a53a7924350c7
Parag Baxi <ghee22@gmail.com>
2013-07-03 06:54:18 -0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+''' Module to hold global settings reused throughout qualysconnect. '''
+
+__author__ = "Colin Bell <colin.bell@uwaterloo.ca>"
+__copyright__ = "Copyright 2011-2013, University of Waterloo"
+__license__ = "BSD-new"
+
+global defaults
+global default_filename
+
+default_filename = ".qcrc"
+
+defaults = { 'hostname' : 'qualysapi.qualys.com' }
