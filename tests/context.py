1f039b0d3d93a61f177d9b84e23646a97c22f932
Gabriel <gabriel59kg@gmail.com>
2018-12-05 06:17:37 -0500

    add test stub

---
+++ tests/context.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+context.py
+~~~~~~~~~~
+Access main module from tests folder
+"""
+import os
+import sys
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../qualysapi"))
+)
+
+print("USING context.py")
+try:
+    import qualysapi
+except ModuleNotFoundError as E:
+    print(E)
+
+
+if __name__ == "__main__":
+    print(os.path.abspath(os.path.join(os.path.dirname(__file__), "../qualysapi")))
+    print(__doc__)
+    print("\tMODULES")
+    print(qualysapi.__doc__)
---
+++ tests/test_smoke.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from sys import version_info
+import importlib
+
+PYTHON_VER = (version_info.major, version_info.minor)
+
+if PYTHON_VER == (2, 7):
+    import pathlib2 as pathlib
+else:
+    import pathlib
+
+import pytest
+
+HERE = pathlib.Path(__file__)
+TEST_ROOT = pathlib.Path(HERE.parent).resolve()
+PKG_ROOT = pathlib.Path(TEST_ROOT.parent).resolve()
+ROOT = pathlib.Path(PKG_ROOT.parent).resolve()
+
+
+def test_qualysapi_context_import():
+    from context import qualysapi
+
+
+def test_qualysapi_regular_import():
+    import qualysapi
+
+
+# @pytest.mark.parametrize("pkg", ["lxml", "requests"])
+# def test_pkgs_not_required(pkg):
+#     pass
+
+
+if __name__ == "__main__":
+    pytest.main(
+        args=[
+            "-vv",
+            "--cov-report",
+            "term",
+            "--cov-report",
+            "xml",
+            "--cov=qualysapi",
+            "tests/",
+        ]
+    )
