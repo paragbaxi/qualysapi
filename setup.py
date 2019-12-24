97d09b0c0faf85a5d4a3b581872efc73e8cad501
Carlos Meza <carlos@digitalr00ts.com>
2019-12-24 11:27:03 -0800

    refactor: setuptools metadata and options to setup.cfg

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
-
 from pathlib import Path
+
 from pkg_resources import VersionConflict, require
 from setuptools import setup
 
@@ -13,35 +13,15 @@ try:
 except VersionConflict:
     sys.exit(f"Error: version of setuptools is too old (<{SETUPTOOLS_VER})!")
 
-__author__ = 'Parag Baxi <parag.baxi@gmail.com>'
-__copyright__ = 'Copyright 2011-2018, Parag Baxi'
-__license__ = 'BSD-new'
+__author__ = "Parag Baxi <parag.baxi@gmail.com>"
+__copyright__ = "Copyright 2011-2018, Parag Baxi"
+__license__ = "BSD-new"
 # Make pyflakes happy.
 __pkgname__ = None
 __version__ = None
 
 exec(Path("qualysapi/version.py").read_text())
 
-setup(name=__pkgname__,
-      version=__version__,
-      author='Parag Baxi',
-      author_email='parag.baxi@gmail.com',
-      description='Qualys API Package',
-      license='BSD-new',
-      keywords='Qualys API helper network security',
-      url='https://github.com/paragbaxi/qualysapi',
-      package_dir={'': '.'},
-      packages=['qualysapi', ],
-      # package_data={'qualysapi':['LICENSE']},
-      # scripts=['src/scripts/qhostinfo.py', 'src/scripts/qscanhist.py', 'src/scripts/qreports.py'],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Topic :: Utilities',
-          'License :: OSI Approved :: Apache Software License',
-          'Intended Audience :: Developers',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-    ],
+setup(
+    name=__pkgname__, version=__version__, license=__license__,
 )
